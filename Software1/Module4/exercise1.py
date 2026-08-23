b72848dbd91ff5bb29c828f25cb5ffee9ed9b27e
Paing Min Thant <paingminthant2010@gmail.com>
2026-08-23 13:54:11 +0300

    Add solutions for Mod4 ex1

--- Software1/Module4/exercise1.py
+++ Software1/Module4/exercise1.py
@@ -1,9 +1,9 @@
 ZanderLength=float(input("Enter the length of the zander in centimeters: "))
-missing_cm= 42-ZanderLength
+missing_cm=42-ZanderLength
 if ZanderLength >=42 :
     print("The zander meets the size limit.")
 
 else :
     print("The zander does not meet the size limit.")
     print("Please release the fish back into the lake.")
-    print(f"The fish was " + str(missing_cm:.1f) + " centimeters below the size limit.")
+    print(f"The fish was {missing_cm:.1f} centimeters below the size limit.")
